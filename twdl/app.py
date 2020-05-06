57de21e61c3185fa3a241bdb0c379bd05b5b2688
Ruchir Chawdhry <ruchir.github@outlook.com>
2020-05-06 18:38:15 +0530

    Added table and printing of video data
    video data = urls, bitrates, filesizes (so far)

--- twdl/app.py
+++ twdl/app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from cnamedtuple import namedtuple
 from plumbum import cli
 from functools import lru_cache
 from rich.console import Console
-
+from rich.table import Column, Table
 
 from .tokens import Token
+from .query import Query
+from .video import Video
 
 
 class TWDL(cli.Application):
+    console = Console()
 
     VERSION = "0.1"
     PROGNAME = "twdl"
@@ -19,24 +23,53 @@ class TWDL(cli.Application):
     guest_token = cli.Flag(["G", "guest-token"], help="Gets the guest token")
     bearer_token = cli.Flag(["B", "bearer-token"], help="Gets the bearer token")
     video_quality = cli.Flag(["best-quality"])
-    tweet_url = cli.Flag(["T", "tweet"])
+    tweet_id = cli.SwitchAttr(["I", "tweet-id"])
+
+    @cli.switch(["-D", "--download"], str)
+    def download(self, tweet_url):
+        console = self.console
+
+        tweet = Query(tweet_url)
+        videos = Video(tweet.get())
+
+        # table
+        columns = ["#", "Bitrate", "Filesize", "Link"]
+        table = Table(show_header=True, header_style="bold")
+        for column in columns:
+            table.add_column(column)
+
+        for i in range(len(videos)):
+            table.add_row(
+                str(i + 1),
+                videos.bitrate[i],
+                videos.filesize[i] + " KB",
+                videos.urls[i],
+            )
+
+        console.print(table)
 
     def main(self):
-        console = Console()
         token = Token()
+        console = self.console
         console.rule("TWDL - Twitter Video Downloader")
 
         if self.guest_token:
             console.print(
                 f"\n\tGuest Token: [bold green]{token.guest_token}\n[/bold green]\n",
                 highlight=False,
-                markup=True,
             )
 
         if self.bearer_token:
-            console.rule("Bearer Token")
             console.print(
-                f"\n\tBearer Token: [bold green]{token.bearer_token}\n[/bold green]\n"
+                f"\n\tBearer Token: [bold green]{token.bearer_token}\n[/bold green]\n",
+                highlight=False,
+            )
+
+        if self.tweet_id:
+            query = Query(self.tweet_id)
+            console.print(
+                f"\n\tTweet ID: [bold green]{query.tweet_id}\n[/bold green]\n",
+                highlight=False,
             )
 
 
